823c1fbce7b4341cc149bb9fba512d5ade80180c
Ticsilva85 <tiago.liverys@gmail.com>
2026-09-06 17:41:16 -0300

    week 01 assignment updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,16 @@
+// ===== HAMBURGER BUTTON ====== //
+
 const navbutton = document.querySelector('#menu');
+const navmenu = document.querySelector('.nav-menu');
+
+
 
 navbutton.addEventListener('click', () => {
     navbutton.classList.toggle('show');
+    navmenu.classList.toggle('show');
 });
 
+// ===== CURRENT YEAR ===== //
+
+
+// ===== LAST MODIFIED DATE ===== //
