1b2b7c9accb7abd2ed8b3a1ab19e604004b6c04e
Ticsilva85 <tiago.liverys@gmail.com>
2026-09-06 17:49:55 -0300

    week 01 assignment updated

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,12 @@ navbutton.addEventListener('click', () => {
 });
 
 // ===== CURRENT YEAR ===== //
+const currentYear = new Date().getFullYear();
+
+document.querySelector('#currentyear').textContent = currentYear;
 
 
 // ===== LAST MODIFIED DATE ===== //
+const lastModified = document.lastModified;
+
+document.querySelector('#lastModified').textContent = `Last Modification: ${lastModified}`;
