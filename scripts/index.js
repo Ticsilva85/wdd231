2a88ed237c290f3004bdda8d44cfdf1b32e79d0e
Ticsilva85 <tiago.liverys@gmail.com>
2026-09-09 21:45:42 -0300

    Week 01 assignment updated

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,9 @@ const navmenu = document.querySelector('.nav-menu');
 navbutton.addEventListener('click', () => {
     navbutton.classList.toggle('show');
     navmenu.classList.toggle('show');
+
+    const isOpen = navbutton.classList.contains('show');
+    navbutton.setAttribute('Aria-expanded', isOpen);
 });
 
 // ===== CURRENT YEAR ===== //
