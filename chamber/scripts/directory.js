81eba53d08046e4f990c26b2f2b2f2b10296aba5
Ticsilva85 <tiago.liverys@gmail.com>
2026-09-18 18:11:05 -0300

    week 02 member json added and diectory.js improved to create the cards

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -10,3 +10,49 @@ navbutton.addEventListener('click', () => {
     const isOpen = navbutton.classList.contains('show');
     navbutton.setAttribute('Aria-expanded', isOpen);
 })
+
+// ===== LOAD MEMBER DATA ===== //
+
+const directory = document.querySelector('#directory');
+
+async function getMembers() {
+    const response = await fetch('./data/members.json');
+    const data = await response.json();
+
+    displayMembers(data);
+}
+
+// ===== DISPLAY MEMBERS ===== //
+function displayMembers(members) {
+    members.forEach(members => {
+
+        const card = document.createElement('article');
+
+        card.classList.add('member.image');
+
+        card.innerHTML = `
+            <img src="./images/${members.image}"
+                alt = "${members.name}"
+                    loading= "lazy">
+                    
+                    <h2>${members.name}</h2>
+
+                    <p>${members.adress}</p>
+
+                    <p>${members.phone}</p>
+
+                    <p>${members.industry}</p>
+
+                    <a href= "${members.website}" target = "_blank">
+                        Visit Website
+                    </a>
+
+                `;
+
+                directory.appendChild(card);
+    });
+}
+
+// ===== START ===== //
+
+getMembers();
